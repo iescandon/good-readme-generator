26c0a492143af1640b2018a009d42eaa43cbe5f8
Inez Escandon <iescan4@gmail.com>
2020-06-12 08:31:57 -0500

    created all files necessary to begin project

---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// array of questions for user
+const questions = [];
+
+//READ ME SECTIONS
+//TITLE
+//DESCRIPTION
+//TABLE OF CONTENTS
+//INSTALLATION
+//USAGE
+//LICENSE
+//CREDITS
+//TESTS
+//QUESTIONS
+
+//what is your project title? title for readme
+//description of your project?
+//installation instructions?
+//usage info?
+//credits?
+//test instructions?
+//choose a license from a list - badge added near the top of the readme and also added in license name
+//enter github username - add link to github profile in questions section
+//enter email address - added to questions section with instructions on how to reach me with additional questions
+//each link in table of contents takes me to corresponding section of README
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
